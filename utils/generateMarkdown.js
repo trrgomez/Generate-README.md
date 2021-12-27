6c4e7ef15fa70bfcc03169187b63bf6f52667a03
trgomez <88842062+trrgomez@users.noreply.github.com>
2021-12-27 11:36:43 -0600

    updated code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseBadge(license) {
+  return `![Github licence](http://img.shields.io/badge/license-${license}-blue.svg)`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -20,25 +14,69 @@ function generateMarkdown(data) {
   ${data.description}
  `;
 
- let tableOfcontents = `## Table of Contents`;
-
- if (data.installation !== '') { tableOfcontents += `
-* [Installation](#installation)` };
-
-if (data.usage !== '') { tableOfcontents += `
-* [Usage](#usage)` };
-
-if (data.contributing !== '') { tableOfcontents += `
-* [Contributing](#contributing)` };
-
-if (data.tests !== '') { tableOfcontents += `
-* [Tests](#tests)` };
-
+ let tableOfcontents = `
+ ## Table of Contents
+ * [Installation](#installation)
+ * [Usage](#usage)
+ * [Contributing](#contributing)
+ * [Tests](#tests)
+ `;
 
 if (data.license !== '') { tableOfcontents += `
 * [License](#license)` };
 
 markdownTemplete += tableOfcontents;
+
+markdownTemplete +=
+`   
+## Installation    
+${data.installation}
+`;
+
+markdownTemplete +=      
+`   
+## Usage     
+${data.usage}
+`;
+      
+markdownTemplete +=        
+`    
+## Contributing    
+${data.contributing}
+`;
+      
+markdownTemplete +=
+`    
+## Tests  
+${data.tests} 
+`
+
+
+// if license is provided
+if(data.license !== ''){
+markdownTemplete += 
+`
+## License
+${renderLicenseBadge(data.license)}
+
+`
+}
+  
+  
+// Questions section
+  
+let questionSection = 
+`
+  
+## Questions
+  
+contact me at: ${data.email}
+GitHub Profile: [https://github.com/${data.username}](https://github.com/${data.username})
+  
+`
+  markdownTemplete += questionSection
+  
+  return markdownTemplete
 
 }
 
