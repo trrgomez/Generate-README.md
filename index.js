be0a25ab577e7479c9948742db2b70f20b0a805c
trgomez <88842062+trrgomez@users.noreply.github.com>
2021-12-27 09:25:40 -0600

    added packages needed for application

--- index.js
+++ index.js
@@ -1,7 +1,100 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username",
+      validate: (username) => {
+        if (username) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What is your email address? (Required)",
+      name: "email",
+      validate: (email) => {
+        if (email) {
+          return true;
+        } else {
+          console.log("Please enter your email!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What is your project's name? (Required)",
+      name: "title",
+      validate: (title) => {
+        if (title) {
+          return true;
+        } else {
+          console.log("Please enter your project's title!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "Please write a short description of your project? (Required)",
+      name: "description",
+      validate: (description) => {
+        if (description) {
+          return true;
+        } else {
+          console.log("Please enter your project's description!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      message:
+        "What command should you run to install dependencies?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message:
+        "What does the user need to know about using the repo?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message:
+        "What does the user need to know about contributing to the repo?",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message:
+        "What command should be run to run tests?",
+      name: "tests",
+    },
+    {
+      type: "list",
+      message: "What kind of license should your project have?",
+      choices: [
+        "GNU AGPLv3",
+        "GNU GPLv3",
+        "GNU LGPLv3",
+        "Mozilla Public License 2.0",
+        "Apache License 2.0",
+        "MIT License",
+        "Boost Software License 1.0",
+        "The Unlicense",
+      ],
+      name: "license",
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
